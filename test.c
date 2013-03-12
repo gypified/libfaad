ed0191408f88dd2423a161764a3cb5e1fcd63732
Nathan Rajlich <nathan@tootallnate.net>
2013-03-11 18:26:43 -0700

    test.c: print the libfaad2 version

--- test.c
+++ test.c
@@ -3,6 +3,8 @@
 #include <neaacdec.h>
 
 int main () {
+  printf("libfaad2 version: %s\n", FAAD2_VERSION);
+
   unsigned long c = NeAACDecGetCapabilities();
   printf("NeAACDecGetCapabilities() - %lu\n", c);
   return 0;
